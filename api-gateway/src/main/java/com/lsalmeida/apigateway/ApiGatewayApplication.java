b1537446278eec8b5f4288769c101b185915af44
Luís Sérgio Almeida <eng.lsergio@gmail.com>
2024-04-02 13:51:59 -0300

    feat: [api-gateway] Adding gateway with load balancer

---
+++ api-gateway/src/main/java/com/lsalmeida/apigateway/ApiGatewayApplication.java
@@ -0,0 +1,15 @@
+package com.lsalmeida.apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient //Optional
+@SpringBootApplication
+public class ApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
+
+}
