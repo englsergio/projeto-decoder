7e72728b7f9a55d535d26296c7c1266c46897005
Luís Sérgio Almeida <eng.lsergio@gmail.com>
2024-04-02 13:55:17 -0300

    feat: [{authuser, course, notification}] Adding service-registry configurations

--- course/src/main/java/com/lsalmeida/course/CourseApplication.java
+++ course/src/main/java/com/lsalmeida/course/CourseApplication.java
@@ -2,7 +2,9 @@ package com.lsalmeida.course;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient //Optional
 @SpringBootApplication
 public class CourseApplication {
 
